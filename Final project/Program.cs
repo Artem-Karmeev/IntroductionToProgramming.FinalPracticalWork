d4d9352449b5e5779fbc37b2b05de6c7356b481f
Artem Karmeev <skinnynew161@gmail.com>
2022-12-02 01:32:23 +0300

    Added the main part of the algorithm

--- Final project/Program.cs
+++ Final project/Program.cs
@@ -5,3 +5,38 @@
         Console.Write(array[i] + "; ");
     }
 }
+
+Console.WriteLine("Введите размерность массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] inputArray = new string[size];
+
+for(int i = 0; i < inputArray.Length; i++)
+{
+    Console.WriteLine($"Введите значение № {i+1}");
+    inputArray[i] = Console.ReadLine();
+}
+
+int count = 0;
+
+for (int i = 0; i < inputArray.Length; i++)
+{
+    if (inputArray[i].Length <= 3)
+    {
+        count++;
+    }
+}
+
+string[] outputArray = new string[count];
+
+for (int i = 0; i < outputArray.Length; i++)
+{
+    if (inputArray[i].Length <= 3)
+    {
+        outputArray[i] = inputArray[i];
+    }
+}
+
+PrintArraiy(inputArray);
+Console.Write(" -> ");
+PrintArraiy(outputArray);
