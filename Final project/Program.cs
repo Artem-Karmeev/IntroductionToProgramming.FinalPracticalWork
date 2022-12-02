13fff6a30b3f1f60b01de0a51687edd7e308b90f
Artem Karmeev <skinnynew161@gmail.com>
2022-12-02 12:38:55 +0300

    Final 2,1

--- Final project/Program.cs
+++ Final project/Program.cs
@@ -15,10 +15,10 @@ for(int i = 0; i < inputArray.Length; i++)
 {
     Console.WriteLine($"Введите значение № {i+1}: ");
     inputArray[i] =  Console.ReadLine()!;
-    
+
     if (inputArray[i] == null)
     {
-        inputArray[i] = " ";
+        inputArray[i] = "";
     }
 }
 
